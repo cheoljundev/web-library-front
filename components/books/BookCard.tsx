8d23e596a09194b9fb381f9ab2bc82b2eed6fc80
cheoljundev <cheoljundev@gmail.com>
2025-02-12 15:52:46 +0900

    BookCard 불필요한 import 제거

--- components/books/BookCard.tsx
+++ components/books/BookCard.tsx
@@ -1,8 +1,6 @@
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image";
-import {AspectRatio} from "@/components/ui/aspect-ratio";
 
 interface Book {
   id: string
@@ -35,22 +33,5 @@ export default function BookCard({ book }: { book: Book }) {
         </Button>
       </CardFooter>
     </Card>
-/*    <div className="card bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src={book.coverUrl}
-          alt={`Cover of ${book.title}`}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{book.title}</h2>
-        <p className="text-sm">저자: {book.author}</p>
-        <p className="text-sm">ISBN: {book.isbn}</p>
-        <p className="text-sm line-clamp-2">{book.description}</p>
-        <div className="card-actions flex justify-center mt-5">
-          <button className="btn btn-primary">자세히</button>
-        </div>
-      </div>
-    </div>*/
   )
 }
