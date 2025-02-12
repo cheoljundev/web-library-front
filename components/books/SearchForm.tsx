34cc4eefc3ec704e3c9f421a4175a8a6a885f356
cheoljundev <cheoljundev@gmail.com>
2025-02-13 01:11:30 +0900

    불필요한 import 제거

--- components/books/SearchForm.tsx
+++ components/books/SearchForm.tsx
@@ -4,7 +4,7 @@ import {useForm} from "react-hook-form";
 import z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Button} from "@/components/ui/button";
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 
 interface SearchFormProps {
