3ad90d083e307c06d9d2a4cbbe2607fa65372aac
cheoljundev <cheoljundev@gmail.com>
2025-02-15 11:30:24 +0900

    Math.min 사용

--- utils.ts
+++ utils.ts
@@ -1,8 +1,6 @@
-import {min} from "@floating-ui/utils";
-
 export function blockSize (blockSize: number, currentPage: number, totalPage: number) {
   const startPage: number = (currentPage / blockSize) * blockSize + 1;
-  const endPage: number = min(startPage + blockSize, totalPage);
+  const endPage: number = Math.min(startPage + blockSize, totalPage);
   const pageNumbers: number[] = [];
   for (let i = startPage; i < endPage; i++) {
     pageNumbers.push(i);
