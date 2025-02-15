e565cdcc9fc3fbb342e802a31d247941d5d45f08
cheoljundev <cheoljundev@gmail.com>
2025-02-15 09:20:53 +0900

    관리자 페이지 Metadata 수정

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,11 @@
 import AdminAside from "@/components/admin/AdminAside";
 import AdminTitle from "@/components/admin/AdminTitle";
+import {Metadata} from "next";
+
+export const metadata : Metadata = {
+  title: "온라인 도서관 관리자 페이지",
+  description: "관리자 페이지",
+}
 
 export default function AdminLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return (
