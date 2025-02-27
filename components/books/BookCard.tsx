3a9d31eeadafab83cbc7e5769b3faa7469d11428
cheoljundev <cheoljundev@gmail.com>
2025-02-28 02:54:03 +0900

    bookcard style 조정

--- components/books/BookCard.tsx
+++ components/books/BookCard.tsx
@@ -31,25 +31,24 @@ export default function BookCard({ book, admin }: { book: Book, admin:boolean })
   }
 
   return (
-    <Card className="block sm:flex md:block lg:flex items-center justify-around p-4">
-      <CardHeader className="flex-row">
+    <Card className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8 p-5">
+      <CardHeader className="flex-row sm:col-span-2 md:col-span-2">
         <Image
           src={book.coverImage}
+          width={150}
+          height={300}
           alt={`Cover of ${book.bookName}`}
-          width={200}         // 원본 이미지의 너비 (예시)
-          height={150}        // 원본 이미지의 높이 (예시)
-          layout="responsive" // 반응형 이미지 레이아웃 사용
         />
       </CardHeader>
-      <CardContent className="lg:w-[300px]">
+      <CardContent className="flex flex-col justify-center col-span-1 sm:col-span-3 md:col-span-5">
         <CardTitle>{book.bookName}</CardTitle>
         <CardDescription>저자 : {book.author}</CardDescription>
         <CardDescription>isbn : {book.isbn}</CardDescription>
         <CardDescription className="line-clamp-4">{book.description}</CardDescription>
       </CardContent>
-      <CardFooter className="flex justify-start sm:justify-between md:justify-start lg:justify-between gap-2">
+      <CardFooter className="flex gap-2 sm:col-span-2 md:col-span-4">
         <Button asChild>
-          <Link href={`/books/${book.id}`}>자세히</Link>
+          <Link href={`/books/${book.id}`}>보기</Link>
         </Button>
         {admin && (
           <>
